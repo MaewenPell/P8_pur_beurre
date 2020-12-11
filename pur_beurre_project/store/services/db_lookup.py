d20598ae2c4754572f7781fd25cb7ea8290c2046
Maewen Pelletier <maewen.p@gmail.com>
2020-12-11 16:23:06 +0100

    Refactor views into services

--- pur_beurre_project/store/services/db_lookup.py
+++ pur_beurre_project/store/services/db_lookup.py
@@ -1,6 +1,9 @@
-from store.models import Aliment
+from store.models import Aliment, Favorite, User
 from django.db.models import Q
 from difflib import SequenceMatcher
+from django.core.paginator import Paginator
+import numpy as np
+
 
 
 def find_best_match(query: str):
@@ -27,3 +30,81 @@ def find_better_alims(alim: Aliment):
     better_alim = Aliment.objects.filter(db_query).order_by("nutriscore")[0:6]
 
     return better_alim
+
+
+def add_aliment_in_favorite(request):
+    alim_id = request.POST.get('add_alim')
+    current_user = User.objects.get(email=request.user.email)
+    try:
+        alim = Aliment.objects.get(pk=alim_id)
+
+        if not Favorite.objects.filter(user=current_user, aliment=alim).exists():
+            Favorite.objects.create(user=current_user, aliment=alim)
+            return "success"
+        else:
+            return "present"
+    except AttributeError:
+        return "unknown"
+
+
+def get_alim(alim_id: int):
+    alim = Aliment.objects.get(pk=alim_id)
+
+    context = {'alim': alim,
+               'nutrim': {
+                   'fat': np.round(float(alim.fat), 2),
+                   'salt': np.round(float(alim.salt), 2),
+                   'sugar': np.round(float(alim.sugar), 2),
+                   'energy': np.round(float(alim.energy), 2)
+                },
+               }
+    return context
+
+
+def remove_alim_from_favorite(request):
+    try:
+        c = request.POST.get('remove_alim')
+        alim_id = c
+        alim = Aliment.objects.get(pk=alim_id)
+        current_user = User.objects.get(email=request.user.email)
+
+        to_del = Favorite.objects.get(user=current_user, aliment=alim)
+        to_del.delete()
+
+        current_user = User.objects.get(email=request.user.email)
+        Favorite.objects.filter(user=current_user)
+        return "success"
+
+    except Aliment.DoesNotExist:
+        return "unkown"
+
+
+def get_results_from_research(request):
+    query = request.GET.get('search_alim')
+    try:
+        best_match = find_best_match(query)
+        better_alims = find_better_alims(Aliment.objects.get(name=best_match['name']))
+    except Aliment.DoesNotExist:
+        return "unkown"
+
+    context = {
+        'alim': better_alims,
+        'query_alim': best_match['product'],
+    }
+    return context
+
+
+def get_user_favorite_alims(request):
+    fav_alim_user = Favorite.objects.filter(user=User.objects.get(email=request.user.email))
+
+    paginator = Paginator(fav_alim_user, 6)
+
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+
+    context = {
+        "alims": fav_alim_user,
+        "len": len(fav_alim_user),
+        "page_obj": page_object
+    }
+    return context
--- pur_beurre_project/store/views.py
+++ pur_beurre_project/store/views.py
@@ -1,15 +1,10 @@
-import numpy as np
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
-from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 
+import store.services.db_lookup as db_lookup
 from store.forms.forms import SignUpForm
-from store.models import Aliment, Favorite
-from store.services.db_lookup import find_best_match, find_better_alims
-from django.contrib import messages
-
-from django.http import HttpResponseRedirect
 
 
 def index(request):
@@ -17,74 +12,35 @@ def index(request):
 
 
 def detail(request, alim_id):
-    alim = Aliment.objects.get(pk=alim_id)
-
-    context = {'alim': alim,
-               'nutrim': {
-                   'fat': np.round(float(alim.fat), 2),
-                   'salt': np.round(float(alim.salt), 2),
-                   'sugar': np.round(float(alim.sugar), 2),
-                   'energy': np.round(float(alim.energy), 2)
-                },
-               }
-
-    return render(request, 'store/detail.html', context)
+    return render(request, 'store/detail.html', db_lookup.get_alim(alim_id))
 
 
 def add_alim(request):
-    try:
-        alim_id = request.POST.get('add_alim')
-        alim = Aliment.objects.get(pk=alim_id)
-        current_user = User.objects.get(email=request.user.email)
+    message = db_lookup.add_aliment_in_favorite(request)
 
-        if not Favorite.objects.filter(user=current_user, aliment=alim).exists():
-            Favorite.objects.create(user=current_user, aliment=alim)
-            messages.success(request, 'Aliment ajouté aux favoris')
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-        else:
-            messages.error(request, 'Aliment déjà présent dans vos favoris')
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-    except AttributeError:
+    if message == "success":
+        messages.success(request, 'Aliment ajouté aux favoris')
+    elif message == "present":
+        messages.error(request, 'Aliment déjà présent dans vos favoris')
+    else:
         messages.error(request, "Cet aliment n'est pas présent dans notre base de donnée")
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def remove_alim(request):
-    try:
-        c = request.POST.get('remove_alim')
-        alim_id = c
-        alim = Aliment.objects.get(pk=alim_id)
-        current_user = User.objects.get(email=request.user.email)
-
-        to_del = Favorite.objects.get(user=current_user, aliment=alim)
-        to_del.delete()
-
-        current_user = User.objects.get(email=request.user.email)
-        Favorite.objects.filter(user=current_user)
-
+    status = db_lookup.remove_alim_from_favorite(request)
+    if status == "success":
         messages.success(request, 'Aliment retiré des favoris')
-        return redirect('my_aliments')
-    except Aliment.DoesNotExist:
-        # If someone want to remove an aliment without the correct
-        # methods
+    else:
         messages.error(request, "Cet aliment n'est pas présent dans notre base de donnée")
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return redirect('my_aliments')
 
 
 def result(request):
-    query = request.GET.get('search_alim')
-    try:
-        best_match = find_best_match(query)
-        better_alims = find_better_alims(Aliment.objects.get(name=best_match['name']))
-    except Aliment.DoesNotExist:
-        print("passed")
-        messages.error(request, "Cet aliment n'est pas présent dans notre base de donnée")
+    context = db_lookup.get_results_from_research(request)
+    if context == "unkown":
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
-    context = {
-                'alim': better_alims,
-                'query_alim': best_match['product'],
-            }
     return render(request, 'store/result.html', context)
 
 
@@ -97,20 +53,7 @@ def user(request):
 
 def my_aliments(request):
     if request.user.is_authenticated:
-        current_user = User.objects.get(email=request.user.email)
-        fav_alim_user = Favorite.objects.filter(user=current_user)
-
-        paginator = Paginator(fav_alim_user, 6)
-
-        page_number = request.GET.get('page')
-        page_object = paginator.get_page(page_number)
-
-        context = {
-            "alims": fav_alim_user,
-            "len": len(fav_alim_user),
-            "page_obj": page_object
-        }
-
+        context = db_lookup.get_user_favorite_alims(request)
         return render(request, 'store/my_aliments.html', context)
     else:
         return redirect("accounts/login")
