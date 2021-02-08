6c7b7dac63547c6b7e6cf3961b5d197b33f96273
Maewen Pelletier <maewen.p@gmail.com>
2021-02-08 11:45:43 +0100

    test-sc-2

--- pur_beurre_project/settings.py
+++ pur_beurre_project/settings.py
@@ -22,9 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-
-SECRET_KEY = "TEST_SECRET_KEY_DEPL"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 if os.environ.get('ENV') == "PRODUCTION":
     DEBUG = False
